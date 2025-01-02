5390afce434e90bae6ccdfba9385b3071586c6a5
SoyAldo <pereyraosvaldo@live.com.ar>
2025-01-02 16:09:19 -0300

    Logica agregada en el index.js y lenguaje del index.html cambiada a es

--- index.js
+++ index.js
@@ -25,6 +25,23 @@ const cosasQueAprendimos = [
   },
 ];
 
-function main() {}
+function main() {
+  const listaEl = document.querySelector(".lista");
+  for (let algo of cosasQueAprendimos) {
+    const tema = algo.tema;
+    const clase = algo.class;
+    // Creo un nuevo elemento 'li'
+    const cosaEl = document.createElement("li");
+    // Le establezco el texto al elemento 'li'
+    cosaEl.textContent = tema;
+    // Verifico si la clase no es un texto vacio
+    if (clase.length > 0) {
+      // Si no es un texto vacio, agrego la clase al elemento 'li'
+      cosaEl.classList.add(clase);
+    }
+    // Agrego el elemento 'li' a la lista
+    listaEl.appendChild(cosaEl);
+  }
+}
 
 main();
